b9e1b86bf34a326c203510405c29347aa2eeb6a2
MADAN126 <madan20131312@gmail.com>
2026-05-16 20:49:17 +0530

    Create RequestBodyValidationApplication.java

---
+++ RequestBodyValidationApplication.java
@@ -0,0 +1,13 @@
+package com.RequestBodyValidation.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RequestBodyValidationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RequestBodyValidationApplication.class, args);
+	}
+
+}
